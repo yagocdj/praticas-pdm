c58f2aacd27ee5cb9d5c45e7c8ce1b7498d60071
Yago César <yagocdj@gmail.com>
2024-07-21 15:06:03 -0300

    adjusting the Art Gallery's layout

--- MyArtGallery/app/src/main/java/com/example/myartgallery/MainActivity.kt
+++ MyArtGallery/app/src/main/java/com/example/myartgallery/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -22,6 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
@@ -49,7 +53,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ArtGallery(modifier: Modifier = Modifier) {
-    Column (Modifier.fillMaxSize()) {
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(top = 18.dp)
+    ) {
         ImageArea()
         ArtworkDescription()
         DisplayController()
@@ -58,15 +66,16 @@ fun ArtGallery(modifier: Modifier = Modifier) {
 
 @Composable
 fun ImageArea(modifier: Modifier = Modifier) {
-    Surface (
+    Surface(
         modifier = modifier
-            .padding(18.dp),
-        shadowElevation = 40.dp,
+            .padding(18.dp)
+            .fillMaxHeight(0.7f),
+        shadowElevation = 6.dp,
 
-    ) {
+        ) {
         Image(
             modifier = Modifier.padding(12.dp),
-            painter = painterResource(R.drawable.andreea_popa_6st6s6i9igm_unsplash),
+            painter = painterResource(R.drawable.dave_hoefler_lsooggc_5dg_unsplash),
             contentDescription = "Black cat inside of a bed cover."
         )
     }
@@ -74,43 +83,51 @@ fun ImageArea(modifier: Modifier = Modifier) {
 
 @Composable
 fun ArtworkDescription(modifier: Modifier = Modifier) {
-    Column (
+    Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom
     ) {
-        Text(
-            text = "Artwork Title",
-            modifier = modifier,
-            fontWeight = FontWeight.Thin,
-            fontSize = 24.sp
-        )
-        Text(
-            text = "Artwork Artist (year)",
-            modifier = modifier,
-            style = MaterialTheme.typography.bodyMedium
-        )
+        Column(
+            modifier = Modifier
+                .background(Color(0xFFEBEBEB))
+                .padding(vertical = 12.dp, horizontal = 6.dp)
+                .fillMaxWidth(0.6f),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Artwork Title",
+                modifier = modifier,
+                fontWeight = FontWeight.Thin,
+                fontSize = 24.sp
+            )
+            Text(
+                text = "Artwork Artist (year)",
+                modifier = modifier,
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
     }
 }
 
 @Composable
 fun DisplayController(modifier: Modifier = Modifier) {
     val buttonWidth = 110.dp
-    Row (
+    Row(
         modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
+            .fillMaxSize()
+            .padding(bottom = 18.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Bottom
     ) {
         Button(
             modifier = Modifier.width(buttonWidth),
-            shape = RectangleShape,
             onClick = { /*TODO*/ }
         ) {
             Text("Previous")
         }
-        Spacer(modifier = Modifier.size(10.dp))
         Button(
             modifier = Modifier.width(buttonWidth),
-            shape = RectangleShape,
             onClick = { /*TODO*/ }
         ) {
             Text("Next")
