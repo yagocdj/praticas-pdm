0964070f0ca12aaa3083d436224e3c4ee581d4b9
Yago César <yagocdj@gmail.com>
2024-08-04 18:58:44 -0300

    adding the code written for all code lessons

--- ScaffoldExample/app/src/main/java/br/edu/ifpb/pdm/scaffoldexample/MainActivity.kt
+++ ScaffoldExample/app/src/main/java/br/edu/ifpb/pdm/scaffoldexample/MainActivity.kt
@@ -6,20 +6,30 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -28,7 +38,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -61,31 +74,14 @@ fun MyApp(modifier: Modifier = Modifier) {
     val xlargeFontSize = 36.sp
 
     Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    // Using WhatsApp green for the container's color
-                    containerColor = colorResource(id = R.color.whatsapp_green),
-                    titleContentColor = colorResource(id = R.color.white),
-                ),
-                // App's main heading :D
-                title = {
-                    Text(text = "WhatsApp 2", fontWeight = FontWeight.Bold)
-                }
-            )
-        },
+        topBar = { MyAppTopAppBar(title = "WhatsApp 2") },
         bottomBar = {
-            BottomAppBar(
+            MyAppBottomAppBar(
+                modifier = Modifier.fillMaxWidth(),
                 containerColor = colorResource(id = R.color.whatsapp_green),
                 contentColor = colorResource(id = R.color.white),
-            ) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = "See you soon ;)",
-                )
-            }
+                text = "See you soon ;)"
+            )
         },
         floatingActionButton = {
             ExtendedFloatingActionButton(
@@ -132,6 +128,77 @@ fun MyApp(modifier: Modifier = Modifier) {
                     }
                 }
             )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyAppTopAppBar(
+    modifier: Modifier = Modifier,
+    title: String = ""
+) {
+    TopAppBar(
+        colors = topAppBarColors(
+            // Using WhatsApp green for the container's color
+            containerColor = colorResource(id = R.color.whatsapp_green),
+            titleContentColor = colorResource(id = R.color.white),
+        ),
+        // App's main heading :D
+        title = {
+            Text(text = title, fontWeight = FontWeight.Bold)
+        },
+        navigationIcon = {
+            IconButton(onClick = { /* TODO - display a calendar with the release date */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.calendar_add_on),
+                    contentDescription = "Add a reminder for the release date."
+                )
+            }
+        }
+    )
+}
+
+@Composable
+fun MyAppBottomAppBar(
+    modifier: Modifier = Modifier,
+    containerColor: Color = BottomAppBarDefaults.containerColor,
+    contentColor: Color = contentColorFor(containerColor),
+    text: String = ""
+) {
+    BottomAppBar(
+        modifier = modifier,
+        containerColor = containerColor,
+        contentColor = contentColor,
+    ) {
+        Row(modifier = modifier, horizontalArrangement = Arrangement.Center) {
+            IconButtonWithText(
+                onClick = { /*TODO - add a comment for the development team*/ },
+                contentDescription = "Send some feedback to the app developers",
+                icon = painterResource(id = R.drawable.add_comment),
+                buttonText = "Talk to us"
+            )
+        }
+    }
+}
+
+@Composable
+fun IconButtonWithText(
+    onClick: () -> Unit,
+    contentDescription: String,
+    icon: Painter,
+    buttonText: String
+) {
+    ElevatedButton(
+        onClick = onClick,
+        shape =
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(painter = icon, contentDescription = contentDescription)
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(text = buttonText)
         }
     }
 }
