6bc3386d994636b3c44f1d95051b2ae78d756a60
Yago César <yagocdj@gmail.com>
2024-08-17 17:51:08 -0300

    realizando a prática de Firebase

--- Navegacao-Firestore-2_firestore_atividades/app/src/main/java/com/example/navegacao1/ui/telas/TelaCadastro.kt
+++ Navegacao-Firestore-2_firestore_atividades/app/src/main/java/com/example/navegacao1/ui/telas/TelaCadastro.kt
@@ -1,5 +1,7 @@
 package com.example.navegacao1.ui.telas
 
+import android.util.Log
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,14 +11,19 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
@@ -28,47 +35,54 @@ import kotlinx.coroutines.launch
 @Composable
 fun TelaCadastro(modifier: Modifier = Modifier, onRegisterClick: () -> Unit) {
 
+    val context = LocalContext.current
+
     var nome by remember { mutableStateOf("") }
-    var cpf by remember { mutableStateOf("") }
-    var idade by remember { mutableStateOf("") }
     var senha by remember { mutableStateOf("") }
 
-    val mensagem by remember { mutableStateOf<String?>(null) }
+    var mensagem by rememberSaveable { mutableStateOf<String?>(null) }
     val scope = rememberCoroutineScope()
-//    val quantidadeMaxDeDigitos = 3
-//    val regexApenasDigitos = Regex("^\\d*\$")
 
     Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "Cadastro", textAlign = TextAlign.Center)
+        Text(
+            text = "Cadastro",
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
         OutlinedTextField(
             value = nome,
             onValueChange = { nome = it },
             label = { Text(text = "Nome") })
-//        Spacer(modifier = Modifier.height(10.dp))
-//        OutlinedTextField(value = cpf, onValueChange = { cpf = it }, label = { Text(text = "CPF") })
-//        Spacer(modifier = Modifier.height(10.dp))
-//        OutlinedTextField(
-//            value = idade,
-//            onValueChange = {
-//                if (it.isDigitsOnly() && it.length <= quantidadeMaxDeDigitos) idade = it
-//            },
-//            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-//            label = { Text(text = "Idade") }
-//        )
         Spacer(modifier = Modifier.height(10.dp))
         OutlinedTextField(
             value = senha, onValueChange = { senha = it },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            visualTransformation = PasswordVisualTransformation(),
             label = { Text(text = "Senha") }
         )
         Spacer(modifier = Modifier.height(10.dp))
         Button(onClick = {
-            scope.launch(Dispatchers.IO) {
-                usuarioDAO.adicionar(Usuario(nome, senha), callback = { })
-                onRegisterClick()
+            val novoUsuario = Usuario(nome = nome, senha = senha)
+            if (novoUsuario.nome.isBlank()) {
+                mensagem = "Digite um nome válido"
+            } else if (novoUsuario.senha.isBlank()) {
+                mensagem = "Digite uma senha válida"
+            } else {
+                scope.launch(Dispatchers.IO) {
+                    usuarioDAO.adicionar(novoUsuario, callback = { usuario ->
+                        if (usuario != null) {
+                            onRegisterClick()
+                        }
+                    })
+                }
             }
         }) {
             Text(text = "Enviar")
+        }
+        mensagem?.let {
+            LaunchedEffect(it) {
+                Toast.makeText(context, mensagem, Toast.LENGTH_SHORT).show()
+                mensagem = null
+            }
         }
     }
 }
--- Navegacao-Firestore-2_firestore_atividades/app/src/main/java/com/example/navegacao1/ui/telas/TelaPrincipal.kt
+++ Navegacao-Firestore-2_firestore_atividades/app/src/main/java/com/example/navegacao1/ui/telas/TelaPrincipal.kt
@@ -1,18 +1,36 @@
 package com.example.navegacao1.ui.telas
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.navegacao1.R
 import com.example.navegacao1.model.dados.Usuario
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -28,7 +46,7 @@ fun TelaPrincipal(modifier: Modifier = Modifier, onLogoffClick: () -> Unit) {
 
         Button(onClick = {
             scope.launch(Dispatchers.IO) {
-                usuarioDAO.buscar( callback = { usuariosRetornados ->
+                usuarioDAO.buscar(callback = { usuariosRetornados ->
                     usuarios.clear()
                     usuarios.addAll(usuariosRetornados)
                 })
@@ -41,13 +59,33 @@ fun TelaPrincipal(modifier: Modifier = Modifier, onLogoffClick: () -> Unit) {
         }
 
         //Carrega sob demanda à medida que o usuário rola na tela
-        LazyColumn {
+        LazyColumn() {
             items(usuarios) { usuario ->
-                //TODO melhore esse card. Estão colados, e com pouca informação. Deixe mais
-                // elegante.
-                Card(modifier = Modifier.fillMaxWidth()) {
-                    Column {
-                        Text(text = usuario.nome)
+                Spacer(modifier = Modifier.height(10.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                ) {
+
+                    val apresentacao = "Meu nome é ${usuario.nome} e eu amo" +
+                        " a disciplina de Programação para Dispositivos Móveis 😍"
+
+                    Column(modifier = Modifier.padding(10.dp)) {
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            Icon(
+                                painter = painterResource(id = R.drawable.sentiment_very_satisfied),
+                                contentDescription = "Very satisfied face emoji"
+                            )
+                            Spacer(modifier = Modifier.width(6.dp))
+                            Text(
+                                text = usuario.nome,
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        Text(text = apresentacao, modifier = Modifier.padding(top = 8.dp))
                     }
                 }
             }
